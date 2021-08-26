69c61de6a949564f6b0da3a101f98df363b2f2b8
Sameer FA <samervj@gmail.com>
2021-08-27 01:14:23 +0530

    PWA optimizations

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -10,7 +10,6 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read http://bit.ly/CRA-PWA
 
-
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -22,6 +21,7 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
+  //process.env.NODE_ENV === 'production' && 
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -33,7 +33,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
